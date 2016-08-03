1283d045eea8fb49af451958212ba066c91cf322
Nenad Biocanin <nenad.biocanin@endava.net>
2016-08-03 17:06:50 +0200

    build with ant

---
+++ src/library/Property.java
@@ -0,0 +1,12 @@
+package library;
+
+public class Property {
+	
+	public static String username = "ivecluj1";
+	public static String password = "IveParola2";
+			
+	public static String username1 = "ivecluj2";
+	public static String password2 = "IveParola2";
+	
+	public static final String URL = "https://www.endavauniversity.com";
+}
